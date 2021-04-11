85d23dd3da270a6f9121db5de6c9773217c00ff7
62011019 <p.pluemapirak@gmail.com>
2021-04-11 23:46:37 +0700

    =njcx]nh,

--- test22.py
+++ test22.py
@@ -1,9 +1,6 @@
 from test2 import validateLogin
 from test3 import *
-<<<<<<< HEAD
+
 print('dddd')
 print("555555ssssssssssssssssssssssss5")
-=======
-print('aaaaaa')
-print('dddddddddddddddd')
->>>>>>> 303be75c1efb8aa6431bf5621fc5103934318b1c
+print("Apirak Oulis")
