36be3bff4be0121fead0bcb3bb52498e10889886
Noppa_W <82326539+noppa001@users.noreply.github.com>
2021-04-11 23:50:34 +0700

    ss

--- test22.py
+++ test22.py
@@ -1,22 +1,19 @@
 from test2 import validateLogin
 from test3 import *
-<<<<<<< HEAD
+
 print('dddd')
 print("555555ssssssssssssssssssssssss5")
 
 
 print("NOPPAWAT")
-=======
+
 
 print('dddd')
 print("555555ssssssssssssssssssssssss5")
-<<<<<<< HEAD
+
 print("Apirak Oulis")
-=======
-=======
+
 print('aaaaaa')
 print('dddddddddddddddd')
->>>>>>> 303be75c1efb8aa6431bf5621fc5103934318b1c
+
 print("P'PAIJA")
->>>>>>> 12a4a7dffb6bb5a508246ca6f9268d573bbb72cc
->>>>>>> 0629770dd7284d35c63bae2795a38a851b9e7811
