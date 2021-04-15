9a13213d542110d101f6cc9a0a1fc9ab6225866c
62011019 <p.pluemapirak@gmail.com>
2021-04-16 02:59:06 +0700

    Update

--- 1.Login_Page/main.py
+++ 1.Login_Page/main.py
@@ -1,11 +1,13 @@
 #import modules
 
+import tkinter
 from tkinter import *
-import os
-from PIL import ImageTk, Image
 from tkinter import messagebox
-# from tkinter_custom_button import TkinterCustomButton
+from tkinter import font as tkFont
+
+from PIL import ImageTk, Image
 import re
+import os
 
 
 # Designing window for registration
@@ -13,7 +15,7 @@ import re
 def register():
     global register_screen
     global regis_bg
-    
+
     register_screen = Toplevel(main_screen)
     register_screen.title("Register")
     x = (960) - (750/2)
@@ -21,12 +23,11 @@ def register():
     register_screen.geometry("750x600+%d+%d" % (x, y))
 
     regis_bg_path = "1.Login_Page\Picture\REGISTER.png"
-    regis_bg = ImageTk.PhotoImage(Image.open(regis_bg_path).resize((750,600)))
+    regis_bg = ImageTk.PhotoImage(Image.open(regis_bg_path).resize((750, 600)))
 
-    canvas = Canvas(register_screen, width=750, height=600)  
+    canvas = Canvas(register_screen, width=750, height=600)
     canvas.pack(fill="both", expand=True)
-    canvas.create_image(0, 0, image=regis_bg,anchor="nw")
-
+    canvas.create_image(0, 0, image=regis_bg, anchor="nw")
 
     global username
     global password
@@ -60,8 +61,6 @@ def register():
     gender_choice1 = IntVar(register_screen)
     gender_choice2 = IntVar(register_screen)
 
-
-
     canvas.create_text(375, 60, text="Please enter details below", font=myfont)
 
     canvas.create_text(65, 150, text="Username *", font=myfont)
@@ -86,10 +85,14 @@ def register():
     canvas.create_window(340, 250, window=lastname_entry, width=180)
 
     canvas.create_text(600, 225, text="Gender *", font=myfont)
-    gender_choice1 = Checkbutton(register_screen, text="Male", font=(myfont, 12), command=my_upd, variable=gender1)
-    gender_choice2 = Checkbutton(register_screen, text="Female", font=(myfont, 12), command=my_upd, variable=gender2)
-    gender_choice1 = Checkbutton(register_screen, text="Male",font=(myfont, 10), command=my_upd, variable=gender1)
-    gender_choice2 = Checkbutton(register_screen, text="Female", font=(myfont, 10), command=my_upd, variable=gender2)
+
+    
+    gender_choice1 = Checkbutton(register_screen, text="Male", font=(
+        myfont, 12), command=my_upd, variable=gender1)
+    gender_choice2 = Checkbutton(register_screen, text="Female", font=(
+        myfont, 12), command=my_upd, variable=gender2)
+   
+        
     canvas.create_window(550, 250, window=gender_choice1)
     canvas.create_window(650, 250, window=gender_choice2)
 
@@ -97,14 +100,13 @@ def register():
     email_entry = Entry(register_screen, textvariable=email)
     canvas.create_window(150, 335, window=email_entry, width=250)
 
-    regis_button = Button(register_screen, text="Register", width=20,height=3, bg="blue", fg="white", command=register_user)
+    helv20 = tkFont.Font(family='Helvetica', size=20, weight=tkFont.BOLD)
+    regis_button = Button(register_screen, text="Register",
+                          font=helv20, bg="blue", fg="white", command=register_user)
     canvas.create_window(400, 420, window=regis_button)
-    clear_button = Button(register_screen, text="Clear", width=20,height=3, bg="blue", fg="white", command=clear_user)
+    clear_button = Button(register_screen, text="Clear",
+                          font=helv20, bg="blue", fg="white", command=clear_user)
     canvas.create_window(600, 420, window=clear_button)
-    
-  
-    
-
 
 def my_upd():
     i = 0
@@ -120,14 +122,6 @@ def my_upd():
     else:
         gender_choice1.config(state='normal')
         gender_choice2.config(state='normal')
-# c1_v=tk.IntVar(my_w)
-# c1=tk.Checkbutton(my_w,text='Python',command=my_upd,variable=c1_v)
-# c1.grid(row=1,column=1)
-
-# c2_v=tk.IntVar(my_w)
-# c2=tk.Checkbutton(my_w,text='PHP',command=my_upd,variable=c2_v)
-# c2.grid(row=1,column=2)
-
 
 # Designing window for login
 
@@ -188,33 +182,41 @@ def register_user():
     email_regex = re.compile(r"[^@]+@[^@]+\.[^@]+")
 
     if (username_info == ''):
-        messagebox.showinfo("Info", "Please Enter Username",parent=register_screen)
+        messagebox.showinfo("Info", "Please Enter Username",
+                            parent=register_screen)
 
     elif (password_info == ''):
-        messagebox.showinfo("Info", "Please Enter Password",parent=register_screen)
-    
+        messagebox.showinfo("Info", "Please Enter Password",
+                            parent=register_screen)
+
     elif (confpassword_info == ''):
-        messagebox.showinfo("Info", "Please Enter Confirm Password",parent=register_screen)
+        messagebox.showinfo(
+            "Info", "Please Enter Confirm Password", parent=register_screen)
 
     elif (password_info != confpassword_info):
-        messagebox.showerror("Error", "Password Not Match",parent=register_screen)
+        messagebox.showerror("Error", "Password Not Match",
+                             parent=register_screen)
         password_entry.delete(0, END)
         confpassword_entry.delete(0, END)
-    
+
     elif (name_info == ''):
-        messagebox.showinfo("Info", "Please Enter First Name",parent=register_screen)
-    
+        messagebox.showinfo(
+            "Info", "Please Enter First Name", parent=register_screen)
+
     elif (lastname_info == ''):
-        messagebox.showinfo("Info", "Please Enter Last Name",parent=register_screen)
-    
+        messagebox.showinfo("Info", "Please Enter Last Name",
+                            parent=register_screen)
+
     elif (gender1.get() == 0 and gender2.get() == 0):
-        messagebox.showinfo("Error", "Please Select Your Gender",parent=register_screen)
+        messagebox.showinfo(
+            "Error", "Please Select Your Gender", parent=register_screen)
 
     elif (email_info == ''):
-        messagebox.showinfo("Info", "Please Enter Your Email",parent=register_screen)
+        messagebox.showinfo(
+            "Info", "Please Enter Your Email", parent=register_screen)
 
     elif (email_regex.match(email_info) == None):
-        messagebox.showerror("Error", "Email Invalid",parent=register_screen)
+        messagebox.showerror("Error", "Email Invalid", parent=register_screen)
 
     else:
         # Write File
@@ -222,15 +224,13 @@ def register_user():
         # file.write(username_info + "\n")
         # file.write(password_info)
         # file.close()
-        
-        if (messagebox.askokcancel("Confirmation", "Are you sure?",parent = register_screen) ) == True:
+
+        if (messagebox.askokcancel("Confirmation", "Are you sure?", parent=register_screen)) == True:
             clear_user()
             register_screen.destroy()
             messagebox.showinfo("Alert", "Register Sucessfully!!")
         else:
             pass
-
-
 
 def login_verify():
     username1 = username_verify.get()
@@ -251,8 +251,8 @@ def login_verify():
     else:
         user_not_found()
 
-# Designing popup for login success
 
+# Designing popup for login success
 
 def login_sucess():
     global login_success_screen
@@ -266,7 +266,6 @@ def login_sucess():
            command=delete_login_success).pack()
 
 # Designing popup for login invalid password
-
 
 def password_not_recognised():
     global password_not_recog_screen
@@ -357,7 +356,8 @@ def main_account_screen():
     canvas.create_image(200, 150, image=img_logo, anchor="nw")
 
     canvas.create_text(280, 350, text="Welcome", font=(myfont, 60), anchor="n")
-    canvas.create_text(320, 430, text="To the land of books",font=(myfont, 20), anchor="n")
+    canvas.create_text(320, 430, text="To the land of books",
+                       font=(myfont, 20), anchor="n")
 
     canvas.create_text(1000, 170, text="Signin", font=(myfont, 40))
 
@@ -383,7 +383,7 @@ def main_account_screen():
     img_regis_path = "1.Login_Page\Picture\\regis-button.png"
     img_regis_button = ImageTk.PhotoImage(
         Image.open(img_regis_path).resize((170, 80)))
-    regis_button = Button(image=img_regis_button,width=140, height=60,
+    regis_button = Button(image=img_regis_button, width=140, height=60,
                           command=register, highlightthickness=0, borderwidth=0)
     canvas.create_window(1100, 450, window=regis_button)
 
@@ -395,7 +395,6 @@ def main_account_screen():
     # logo_path = os.path.join(base_folder,'logo.jpg')
     # img = ImageTk.PhotoImage(Image.open(logo_path).resize((700, 300)))
     # canvas.create_image(0, 0, anchor=NW, image=img)
-    
 
     main_screen.resizable(0, 0)
     main_screen.overrideredirect(0)
