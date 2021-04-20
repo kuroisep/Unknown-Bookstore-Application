bbca85f498258d37aee36bf07dd34feda760384a
62011019 <p.pluemapirak@gmail.com>
2021-04-20 21:50:13 +0700

    .

--- 2_Shop_Page/Main.py
+++ 2_Shop_Page/Main.py
@@ -10,14 +10,14 @@ x = (700) - (750/2)
 y = (420) - (500/2)
 shop_window.geometry("1280x720+%d+%d" % (x, y))
 
-bg_path = "PICTURE\Shop_bg.png"
+bg_path = "2_Shop_Page\PICTURE\Shop_bg.png"
 bg = ImageTk.PhotoImage(Image.open(bg_path).resize((1280, 720)))
 
 canvas = Canvas(shop_window, width=1280, height=720)
 canvas.pack(fill="both", expand=True)
 canvas.create_image(0, 0, image=bg,anchor="nw")
 
-logo_path = "PICTURE\logo.png"
+logo_path = "2_Shop_Page\PICTURE\logo.png"
 logo = ImageTk.PhotoImage(Image.open(logo_path).resize((150, 150)))
 
 name = tk.StringVar()
@@ -36,9 +36,9 @@ label1 = tk.Label(image=logo)
 label1.image = logo
 label1.place(x=0, y=0)
 
-banner1_path = "PICTURE\\banner1.jpg"
-banner2_path = "PICTURE\\banner2.jpg"
-banner3_path = "PICTURE\\banner3.jpg"
+banner1_path = "2_Shop_Page\PICTURE\\banner1.jpg"
+banner2_path = "2_Shop_Page\PICTURE\\banner2.jpg"
+banner3_path = "2_Shop_Page\PICTURE\\banner3.jpg"
 banner1 = ImageTk.PhotoImage(Image.open(banner1_path))
 banner2 = ImageTk.PhotoImage(Image.open(banner2_path))
 banner3 = ImageTk.PhotoImage(Image.open(banner3_path))
@@ -46,9 +46,9 @@ banner3 = ImageTk.PhotoImage(Image.open(banner3_path))
 banner_label = tk.Label(shop_window)
 banner_label.pack()
 
-dot1_path = "PICTURE\movingdot1.png"
-dot2_path = "PICTURE\movingdot2.png"
-dot3_path = "PICTURE\movingdot3.png"
+dot1_path = "2_Shop_Page\PICTURE\movingdot1.png"
+dot2_path = "2_Shop_Page\PICTURE\movingdot2.png"
+dot3_path = "2_Shop_Page\PICTURE\movingdot3.png"
 dot1 = ImageTk.PhotoImage(Image.open(dot1_path))
 dot2 = ImageTk.PhotoImage(Image.open(dot2_path))
 dot3 = ImageTk.PhotoImage(Image.open(dot3_path))
