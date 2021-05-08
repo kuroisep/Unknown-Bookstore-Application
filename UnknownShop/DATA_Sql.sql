80d0c8cbf9b88277ccde979e07c6f83925fc7479
62011019 <p.pluemapirak@gmail.com>
2021-05-08 21:02:25 +0700

    sql

---
+++ UnknownShop/DATA_Sql.sql
@@ -0,0 +1,14 @@
+create table books(
+No int,
+Code varchar(20),
+Book_No int,
+Name varchar(100),
+Author varchar(100),
+Ex varchar(3000),
+Price varchar(100),
+Page varchar(100),
+Category varchar(100),
+Language varchar(100),
+Stocks varchar(100),
+Rating varchar(100)
+);
